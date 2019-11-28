f94989bf5736b1d0d2c3ab2d0a671d816102c876
KarolKasprzak <staineer@gmail.com>
2019-11-29 00:37:18 +0100

    v0.1

--- src/com/cosma/Converter.java
+++ src/com/cosma/Converter.java
@@ -69,7 +69,7 @@ class Converter {
                 BufferedImage.TYPE_INT_RGB);
 
         Graphics2D graphic = result.createGraphics();
-        Font font = new Font("Droid Sans Mono", Font.PLAIN, 9);
+        Font font = new Font("Droid Sans Mono", Font.PLAIN, 8);
         graphic.setFont(font);
         graphic.setColor(Color.GREEN);
 
@@ -78,8 +78,8 @@ class Converter {
 
         //Convert to grayscale & collect date to draw
 
-        int pH = 8;
-        int pW = 6;
+        int pH = 9;
+        int pW = 10;
         for (int i = 0; i < result.getHeight() / pH; i++) {
             for (int j = 0; j < result.getWidth() / pW; j++) {
                 float averageBrightness = 0;
@@ -97,7 +97,7 @@ class Converter {
                     }
                 }
                 averageBrightness = averageBrightness / (pH * pW);
-                averageBrightness = round(averageBrightness, 1);
+                averageBrightness = round(averageBrightness, 2);
                 AsciiData asciiData = new AsciiData(lastX, lastY, averageBrightness);
                 asciiArray.add(asciiData);
             }
@@ -112,33 +112,77 @@ class Converter {
 
         //Draw ascii
         for (AsciiData asciiData : asciiArray) {
-            if (asciiData.luminance <= 0.1f && asciiData.luminance >= 0.0f) {
+//
+
+//            if (asciiData.luminance <= 0.4f && asciiData.luminance >= 0.2f) {
+//                graphic.drawString("!", asciiData.x, asciiData.y);
+//            }
+//            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.5f) {
+//                graphic.drawString("*", asciiData.x, asciiData.y);
+//            }
+            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.15f) {
                 graphic.drawString("0", asciiData.x, asciiData.y);
+                continue;
             }
-            if (asciiData.luminance == 0.2f) {
-                graphic.drawString("@", asciiData.x, asciiData.y);
+//            if (asciiData.luminance <= 0.35f && asciiData.luminance >= 0.25f) {
+//                graphic.drawString("t", asciiData.x, asciiData.y);
+//                continue;
+//            }
+//            if (asciiData.luminance <= 0.45f && asciiData.luminance >= 0.35f) {
+//                graphic.drawString("q", asciiData.x, asciiData.y);
+//                continue;
+//            }
+//            if (asciiData.luminance <= 0.5f && asciiData.luminance >= 0.45f) {
+//                graphic.drawString("U", asciiData.x, asciiData.y);
+//                continue;
+//            }
+//            if (asciiData.luminance <= 0.55f && asciiData.luminance >= 0.5f) {
+//                graphic.drawString("n", asciiData.x, asciiData.y);
+//                continue;
+//            }
+            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.55f) {
+                graphic.drawString("j", asciiData.x, asciiData.y);
+                continue;
             }
-            if (asciiData.luminance == 0.3f) {
-                graphic.drawString("$", asciiData.x, asciiData.y);
+            if (asciiData.luminance <= 0.65f && asciiData.luminance >= 0.6f) {
+                graphic.drawString("/", asciiData.x, asciiData.y);
+                continue;
             }
-            if (asciiData.luminance == 0.4f) {
-                graphic.drawString("%", asciiData.x, asciiData.y);
-            }
-            if (asciiData.luminance == 0.5f) {
-                graphic.drawString("*", asciiData.x, asciiData.y);
-            }
-            if (asciiData.luminance == 0.6) {
-                graphic.drawString(";", asciiData.x, asciiData.y);
-            }
-            if (asciiData.luminance == 0.7f) {
-                graphic.drawString(",", asciiData.x, asciiData.y);
-            }
-            if (asciiData.luminance == 0.8f) {
+            if (asciiData.luminance <= 0.75f && asciiData.luminance >= 0.65f) {
                 graphic.drawString("^", asciiData.x, asciiData.y);
+                continue;
             }
-            if (asciiData.luminance == 0.9f) {
+            if (asciiData.luminance <= 0.85f && asciiData.luminance >= 0.75f) {
+                graphic.drawString(";", asciiData.x, asciiData.y);
+                continue;
+            }
+            if (asciiData.luminance <= 0.9f && asciiData.luminance >= 0.85f) {
+                graphic.drawString(",", asciiData.x, asciiData.y);
+                continue;
+            }
+            if (asciiData.luminance <= 0.97f && asciiData.luminance >= 0.9f) {
                 graphic.drawString(".", asciiData.x, asciiData.y);
+
             }
+//            if (asciiData.luminance <= 1f && asciiData.luminance >= 0.97f) {
+//                graphic.drawString("0", asciiData.x, asciiData.y);
+//            }
+
+//            if (asciiData.luminance <= 0.4f && asciiData.luminance >= 0.2f) {
+//                graphic.drawString("!", asciiData.x, asciiData.y);
+//            }
+//            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.5f) {
+//                graphic.drawString("*", asciiData.x, asciiData.y);
+//            }
+//            if (asciiData.luminance <= 0.7f && asciiData.luminance >= 0.6f) {
+//                graphic.drawString("i", asciiData.x, asciiData.y);
+//            }
+//            if (asciiData.luminance <= 0.86f && asciiData.luminance >= 0.7f) {
+//                graphic.drawString(",", asciiData.x, asciiData.y);
+//            }
+//            if (asciiData.luminance <= 0.97f && asciiData.luminance >= 0.f) {
+//                graphic.drawString(".", asciiData.x, asciiData.y);
+//            }
         }
 
         File output = new File("convert_" + imageName);
--- src/com/cosma/Main.java
+++ src/com/cosma/Main.java
@@ -9,6 +9,6 @@ public class Main {
 
         System.out.println();
         Converter converter = new Converter();
-        System.in.read();
+//        System.in.read();
     }
 }
