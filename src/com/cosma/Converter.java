571b3d0b0226332398969814c1b80e43abd86535
KarolKasprzak <staineer@gmail.com>
2019-12-06 00:17:06 +0100

    v0.2

--- src/com/cosma/Converter.java
+++ src/com/cosma/Converter.java
@@ -4,19 +4,22 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
 class Converter {
+
+    private HashMap<Integer,String> asciiMap;
+
 
     class AsciiData {
         int x;
@@ -35,6 +38,9 @@ class Converter {
     Converter() throws IOException {
 
         //Find all images
+
+        asciiMap = new HashMap<>();
+        String[] characters = {" ",".",":",",",";","o","x","%","#","@"};
 
         try (Stream<Path> walk = Files.walk(Paths.get(""))) {
 
@@ -69,7 +75,7 @@ class Converter {
                 BufferedImage.TYPE_INT_RGB);
 
         Graphics2D graphic = result.createGraphics();
-        Font font = new Font("Droid Sans Mono", Font.PLAIN, 8);
+        Font font = new Font("Consolas", Font.PLAIN, 8);
         graphic.setFont(font);
         graphic.setColor(Color.GREEN);
 
@@ -78,8 +84,8 @@ class Converter {
 
         //Convert to grayscale & collect date to draw
 
-        int pH = 9;
-        int pW = 10;
+        int pH = 12;
+        int pW = 14;
         for (int i = 0; i < result.getHeight() / pH; i++) {
             for (int j = 0; j < result.getWidth() / pW; j++) {
                 float averageBrightness = 0;
@@ -90,14 +96,15 @@ class Converter {
                 for (int i1 = i * pH; i1 < i * pH + pH; i1++) {
                     for (int j1 = j * pW; j1 < j * pW + pW; j1++) {
                         Color c = new Color(result.getRGB(j1, i1));
-                        float luminance = (c.getRed() * 0.2126f + c.getGreen() * 0.7152f + c.getBlue() * 0.0722f) / 255;
+//                        float luminance = (c.getRed() * 0.21f + c.getGreen() * 0.71f + c.getBlue() * 0.07f) / 255;
+                        float luminance = (c.getRed() * 0.21f + c.getGreen() * 0.71f + c.getBlue() * 0.07f);
                         averageBrightness = averageBrightness + luminance;
                         lastX = j1;
                         lastY = i1;
                     }
                 }
                 averageBrightness = averageBrightness / (pH * pW);
-                averageBrightness = round(averageBrightness, 2);
+                averageBrightness = round(averageBrightness, 1);
                 AsciiData asciiData = new AsciiData(lastX, lastY, averageBrightness);
                 asciiArray.add(asciiData);
             }
@@ -112,35 +119,12 @@ class Converter {
 
         //Draw ascii
         for (AsciiData asciiData : asciiArray) {
-//
-
-//            if (asciiData.luminance <= 0.4f && asciiData.luminance >= 0.2f) {
-//                graphic.drawString("!", asciiData.x, asciiData.y);
-//            }
-//            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.5f) {
-//                graphic.drawString("*", asciiData.x, asciiData.y);
-//            }
-            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.15f) {
-                graphic.drawString("0", asciiData.x, asciiData.y);
-                continue;
-            }
-//            if (asciiData.luminance <= 0.35f && asciiData.luminance >= 0.25f) {
-//                graphic.drawString("t", asciiData.x, asciiData.y);
+//            graphic.drawString(String.valueOf(asciiData.luminance), asciiData.x, asciiData.y);
+//            if (asciiData.luminance <= 0.2f && asciiData.luminance >= 0.08f) {
+//                graphic.drawString("''", asciiData.x, asciiData.y);
 //                continue;
 //            }
-//            if (asciiData.luminance <= 0.45f && asciiData.luminance >= 0.35f) {
-//                graphic.drawString("q", asciiData.x, asciiData.y);
-//                continue;
-//            }
-//            if (asciiData.luminance <= 0.5f && asciiData.luminance >= 0.45f) {
-//                graphic.drawString("U", asciiData.x, asciiData.y);
-//                continue;
-//            }
-//            if (asciiData.luminance <= 0.55f && asciiData.luminance >= 0.5f) {
-//                graphic.drawString("n", asciiData.x, asciiData.y);
-//                continue;
-//            }
-            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.55f) {
+            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.35f) {
                 graphic.drawString("j", asciiData.x, asciiData.y);
                 continue;
             }
@@ -164,25 +148,6 @@ class Converter {
                 graphic.drawString(".", asciiData.x, asciiData.y);
 
             }
-//            if (asciiData.luminance <= 1f && asciiData.luminance >= 0.97f) {
-//                graphic.drawString("0", asciiData.x, asciiData.y);
-//            }
-
-//            if (asciiData.luminance <= 0.4f && asciiData.luminance >= 0.2f) {
-//                graphic.drawString("!", asciiData.x, asciiData.y);
-//            }
-//            if (asciiData.luminance <= 0.6f && asciiData.luminance >= 0.5f) {
-//                graphic.drawString("*", asciiData.x, asciiData.y);
-//            }
-//            if (asciiData.luminance <= 0.7f && asciiData.luminance >= 0.6f) {
-//                graphic.drawString("i", asciiData.x, asciiData.y);
-//            }
-//            if (asciiData.luminance <= 0.86f && asciiData.luminance >= 0.7f) {
-//                graphic.drawString(",", asciiData.x, asciiData.y);
-//            }
-//            if (asciiData.luminance <= 0.97f && asciiData.luminance >= 0.f) {
-//                graphic.drawString(".", asciiData.x, asciiData.y);
-//            }
         }
 
         File output = new File("convert_" + imageName);
